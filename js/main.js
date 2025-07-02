ea7ca33514685b376a398463ddbaee39be5da042
Lucianolam <153454794+Lucianolam@users.noreply.github.com>
2025-07-02 10:40:53 -0300

    ejercicios matematicos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-let materia = "javascript"
-let comision = 88115
+//let materia = "javascript"
+//let comision = 88115
 let flex = true
 
 //console.log ("materia" + materia)
 
-let materia= prompt("ingrese la materia que va a cursar")
-let comision= prompt("ingrese el numero de comision")
+//let materia = prompt("ingrese la materia que va a cursar")
+//let comision = prompt("ingrese el numero de comision")
+//alert (" materia=  " +  materia +     "   comision=  " +  comision)
+
+let numeroA = parseInt (prompt ("ingreso el primer numero"))
+let numeroB = parseInt (prompt ("ingrese el segundo numero"))
+
+let resultado = numeroA + numeroB
+console.log (resultado)
